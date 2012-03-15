667e69a4139db6006d0e19f8018bec60bc4d0e2c
Justin Bayer <bayer.justin@googlemail.com>
2012-03-15 10:45:25 +0100

    Added until module with ParameterSet and Model class.

---
+++ breze/util.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+
+import contextlib
+import sys
+import time
+
+import numpy as np
+import theano.tensor as T
+import theano
+
+
+class ParameterSet(object):
+
+    def __init__(self, **kwargs):
+        # Make sure all size specifications are tuples.
+        kwargs = dict((k, v if isinstance(v, tuple) else (v,))
+                      for k, v in kwargs.iteritems())
+        # Find out total size of needed parameters and create memory for it.
+        sizes = [np.prod(i) for i in kwargs.values()]
+        self.n_pars = sum(sizes)
+        self.flat = theano.shared(np.empty(self.n_pars), name='flat')
+        # Go through parameters and assign space and variable.
+        self.views = {}
+        n_used = 0 	# Number of used parameters.
+        self.data = self.flat.get_value(borrow=True, return_internal_type=True)
+        for (key, shape), size in zip(kwargs.items(), sizes):
+            # Make sure the key is legit -- that it does not overwrite anything.
+            if hasattr(self, key):
+                raise ValueError("%s is an illegal name for a variable")
+  
+            # Get the region from the big flat array.
+            region = self.data[n_used:n_used + size]
+            # Then shape it correctly and make it accessible from the outside.
+            region.shape = shape
+            self.views[key] = region
+  
+            # Get the right variable as a subtensor.
+            var = self.flat[n_used:n_used + size].reshape(shape)
+            var.name = key
+            setattr(self, key, var)
+  
+            n_used += size
+  
+    def __getitem__(self, key):
+        return self.views[key]
+
+    def __setitem__(self, key, value):
+        self.views[key][:] = value
+
+
+class Model(object):
+
+    def __init__(self, exprs, parameters):
+        self.exprs = exprs
+        self.parameters = parameters
+
+    def function(self, variables, exprs, mode=None, explicit_pars=False):
+        """Return a function for the given `exprs` given `variables`.
+
+        If `mode` is different to None, the specified mode is used, otherwise
+        the Theano default. In case `explicit_pars` is set to True, the first
+        argument of the function needs to be a numpy array from which the
+        parameters of the LossBased model will be extracted."""
+        def lookup(varname):
+            res = getattr(self.parameters, varname, None)
+            if res is None:
+                res = self.exprs[i]
+            return res
+        variables = [lookup(i) if isinstance(i, str) else i
+                     for i in variables]
+
+        if mode is None:
+            mode = theano.Mode(linker='cvm')
+
+        if isinstance(exprs, (str, unicode)):
+            # We are only being given a single string expression.
+            exprs = self.exprs[exprs]
+        elif isinstance(exprs, theano.tensor.basic.TensorVariable):
+            exprs = exprs
+        else:
+            # We have several, either string or variable, thus make it a list
+            # and substitute the strings.
+            exprs = list(exprs)
+            exprs = [self.exprs[i] if isinstance(i, str) else i for i in exprs]
+
+        if explicit_pars:
+            pars = T.dvector(self.parameters.flat.name + '-substitute')
+            variables = [pars] + variables
+            givens = [(self.parameters.flat, pars)]
+        else:
+            givens = []
+
+        return theano.function(variables, exprs, givens=givens, mode=mode)
---
+++ test/test_util.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import theano
+import theano.tensor as T
+
+from breze.util import ParameterSet, Model
+from tools import roughly
+
+
+def test_parameter_set_init():
+    pars = ParameterSet(matrix=(10, 10),
+                        vector=10)
+    assert pars.data.shape == (110,), 'wrong size for flat pars allocated'
+    assert (pars['matrix'].shape == (10, 10)), ('wrong size for 2d array in pars '
+        'allocated')
+    assert (pars['vector'].shape == (10,)), ('wrong size for 1d array in pars '
+        'allocated')
+
+
+def test_parameter_set_data_change():
+    pars = ParameterSet(matrix=(10, 10),
+                        vector=10)
+    pars['matrix'] *= 0
+    pars['vector'] *= 0
+    assert (pars.data == 0).all()
+
+    pars['matrix'] += 1
+    assert pars.data.sum() == 100
+
+    pars['vector'] += 2
+    assert pars.data.sum() == 120
+
+    pars.data *= 0.5
+    pars.data.sum() == 60
+
+
+def test_model_function():
+    pars = ParameterSet(weights=(2, 3))
+    inpt = T.matrix()
+    output = T.dot(inpt, pars.weights)
+
+    model = Model({'inpt': inpt, 'output': output}, pars)
+
+    f = model.function(['inpt'], 'output')
+    fx = model.function(['inpt'], 'output', explicit_pars=True)
+
+    np.random.seed(1010)
+    test_inpt = np.random.random((10, 2))
+    correct = roughly(f(test_inpt), fx(pars.data, test_inpt))
+
+    assert correct, 'implicit pars and explicit pars have different output'
+
+    f1 = model.function(['inpt'], ['output'])
+    f2 = model.function([inpt], ['output'])
+    f3 = model.function([inpt], [output])
+    f4 = model.function(['inpt'], [output])
+
+    assert roughly(f1(test_inpt), f2(test_inpt)), "f1 and f2 don't agree"
+    assert roughly(f1(test_inpt), f3(test_inpt)), "f1 and f3 don't agree"
+    assert roughly(f1(test_inpt), f4(test_inpt)), "f1 and f4 don't agree"
+    assert roughly(f2(test_inpt), f3(test_inpt)), "f2 and f3 don't agree"
+    assert roughly(f2(test_inpt), f4(test_inpt)), "f2 and f4 don't agree"
+    assert roughly(f3(test_inpt), f4(test_inpt)), "f3 and f4 don't agree"
