aab31d1c9d39d353c40108b6fbd3243cd48301de
Justin Bayer <bayer.justin@googlemail.com>
2012-03-20 13:18:51 +0100

    added function to normalize with a given norm

--- breze/component/norm.py
+++ breze/component/norm.py
@@ -18,3 +18,17 @@ def root_l2(inpt, axis=None):
 
 def exp(inpt, axis=None):
     return T.exp(inpt).sum(axis=axis)
+
+
+def normalize(inpt, norm, axis, eps=1E-8):
+    if axis not in (0, 1):
+        raise ValueError('only axis 0 or 1 allowed')
+
+    this_norm = norm(inpt, axis=axis)
+
+    if axis == 0:
+        res = inpt / (this_norm + eps)
+    elif axis == 1:
+        res = (inpt.T / (this_norm + eps)).T
+
+    return res
