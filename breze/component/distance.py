ae058f3ab851b3b3b4249e1587def55a3e259ef3
Justin Bayer <bayer.justin@googlemail.com>
2012-03-20 13:19:07 +0100

    clarified (bernoulli)_cross_entropy behavior

--- breze/component/distance.py
+++ breze/component/distance.py
@@ -6,8 +6,12 @@ import theano.tensor as T
 from norm import l1, l2
 
 
-def bernoulli_cross_entropy(X, Y, axis=None):
+def cross_entropy(X, Y, axis=None):
     return -(X * T.log(Y)).sum(axis=axis)
+
+
+def bernoulli_cross_entropy(X, Y, axis=None):
+    return -(X * T.log(Y) + (1 - X) * T.log(1 - Y)).sum(axis=axis)
 
 
 def squared(X, Y, axis=None):
