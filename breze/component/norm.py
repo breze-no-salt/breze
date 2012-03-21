Fixed normalize, for softmax.

breze/component/norm.py
@@ -20,15 +20,15 @@ def exp(inpt, axis=None):
     return T.exp(inpt).sum(axis=axis)
 
 
-def normalize(inpt, norm, axis, eps=1E-8):
+def normalize(inpt, f_comp, axis, eps=1E-8):
     if axis not in (0, 1):
         raise ValueError('only axis 0 or 1 allowed')
 
-    this_norm = norm(inpt, axis=axis)
-
+    transformed = f_comp(inpt)
+    this_norm = transformed.sum(axis=axis)
     if axis == 0:
-        res = inpt / (this_norm + eps)
+        res = transformed / (this_norm + eps)
     elif axis == 1:
-        res = (inpt.T / (this_norm + eps)).T
+        res = (transformed.T / (this_norm + eps)).T
 
     return res