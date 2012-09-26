9f4cca2a06d8358c00795caf153edac7ef34aa19
Justin Bayer <bayer.justin@googlemail.com>
2012-09-26 11:40:35 +0200

    Added supervised/unsupervised LSTM rnns

--- brummlearn/rnn.py
+++ brummlearn/rnn.py
@@ -3,7 +3,7 @@
 """Module for learning various types of recurrent networks."""
 
 
-import itertools 
+import itertools
 
 import breze.model.sequential.rnn as rnn
 import climin
@@ -13,8 +13,8 @@ import theano.tensor as T
 from breze.model.neural import TwoLayerPerceptron
 
 from brummlearn.base import (
-        SupervisedBrezeWrapperBase, UnsupervisedBrezeWrapperBase,
-        TransformBrezeWrapperMixin)
+    SupervisedBrezeWrapperBase, UnsupervisedBrezeWrapperBase,
+    TransformBrezeWrapperMixin)
 
 
 class BaseRnn(object):
@@ -22,16 +22,16 @@ class BaseRnn(object):
     # TODO: default loss should not be squared, which makes reordering ofi
     # arguments necessary.
 
-    def __init__(self, n_inpt, n_hidden, n_output, 
-                 hidden_transfer='tanh', out_transfer='identity', 
+    def __init__(self, n_inpt, n_hidden, n_output,
+                 hidden_transfer='tanh', out_transfer='identity',
                  loss='squared', pooling=None,
-                 optimizer='ksd',
+                 optimizer='rprop',
                  pretrain=False,
                  max_iter=1000,
                  verbose=False):
         """Create and return a ``Rnn`` object.
 
-        :param n_inpt: Number of inputs per time step to the network. 
+        :param n_inpt: Number of inputs per time step to the network.
         :param n_hidden: Size of the hidden state.
         :param n_output: Size of the output of the network.
         :param hidden_transfer: Transfer function to use for the network. This
@@ -79,7 +79,6 @@ class BaseRnn(object):
         output_in = self.exprs['output-in']
         loss = self.exprs['loss']
         flat_pars = self.parameters.flat
-        hidden_in_rec = self.exprs['hidden-in-rec']
         p = self.exprs['some-vector'] = T.vector('some-vector')
 
         Jp = T.Rop(output_in, flat_pars, p)
@@ -92,7 +91,7 @@ class BaseRnn(object):
 
     def _make_loss_functions(self):
         """Return triple (f_loss, f_d_loss, f_Hp) of functions.
-        
+
          - f_loss returns the current loss,
          - f_d_loss returns the gradient of that loss wrt parameters,
          - f_Hp returns the product of an arbitrary vector of the Gauss Newton
@@ -112,7 +111,7 @@ class BaseRnn(object):
 class SupervisedRnn(BaseRnn, rnn.SupervisedRecurrentNetwork,
     SupervisedBrezeWrapperBase):
     """Class implementing recurrent neural networks for supervised learning..
-    
+
     The class inherits from breze's RecurrentNetwork class and adds several
     sklearn like methods.
     """
@@ -122,7 +121,7 @@ class SupervisedRnn(BaseRnn, rnn.SupervisedRecurrentNetwork,
         net = TwoLayerPerceptron(
             self.n_inpt, self.n_hidden, self.n_output,
             self.hidden_transfer, self.out_transfer, self.loss)
-        common_pars = ('in_to_hidden', 'hidden_bias', 
+        common_pars = ('in_to_hidden', 'hidden_bias',
                        'hidden_to_out', 'out_bias')
 
         # Copy parameters to mlp.
@@ -165,7 +164,7 @@ class SupervisedRnn(BaseRnn, rnn.SupervisedRecurrentNetwork,
         the optimization can be broken any time by the caller.
 
         This method does `not` respect the max_iter attribute.
-        
+
         :param X: A (t, n ,d) array where _t_ is the number of time steps,
             _n_ is the number of data samples and _d_ is the dimensionality of
             a data sample at a single time step.
@@ -188,7 +187,7 @@ class SupervisedRnn(BaseRnn, rnn.SupervisedRecurrentNetwork,
 class UnsupervisedRnn(BaseRnn, rnn.UnsupervisedRecurrentNetwork,
     UnsupervisedBrezeWrapperBase, TransformBrezeWrapperMixin):
     """Class implementing recurrent neural networks for unsupervised learning..
-    
+
     The class inherits from breze's RecurrentNetwork class and adds several
     sklearn like methods.
     """
@@ -201,6 +200,21 @@ class UnsupervisedRnn(BaseRnn, rnn.UnsupervisedRecurrentNetwork,
         # MLPs are not implemented for unsupervised problems. Or find a way to
         # efficiently do this without an MLP.
 
+    def _make_loss_functions(self):
+        """Return triple (f_loss, f_d_loss, f_Hp) of functions.
+
+         - f_loss returns the current loss,
+         - f_d_loss returns the gradient of that loss wrt parameters,
+         - f_Hp returns the product of an arbitrary vector of the Gauss Newton
+           matrix of the loss.
+        """
+        d_loss = self._d_loss()
+
+        args = list(self.data_arguments)
+        f_loss = self.function(args, 'loss', explicit_pars=True)
+        f_d_loss = self.function(args, d_loss, explicit_pars=True)
+        return f_loss, f_d_loss
+
     def iter_fit(self, X):
         """Iteratively fit the parameters of the model to the given data with
         the given error function.
@@ -210,13 +224,129 @@ class UnsupervisedRnn(BaseRnn, rnn.UnsupervisedRecurrentNetwork,
         the optimization can be broken any time by the caller.
 
         This method does `not` respect the max_iter attribute.
-        
+
         :param X: A (t, n ,d) array where _t_ is the number of time steps,
             _n_ is the number of data samples and _d_ is the dimensionality of
             a data sample at a single time step.
         """
         if self.pretrain:
             self._pretrain(X)
+
+        #f_loss, f_d_loss, f_Hp = self._make_loss_functions()
+        f_loss, f_d_loss = self._make_loss_functions()
+
+        args = itertools.repeat(([X], {}))
+        opt = self._make_optimizer(f_loss, f_d_loss, args)
+
+        for i, info in enumerate(opt):
+            yield info
+
+
+
+class BaseLstm(BaseRnn):
+
+    def __init__(self, n_inpt, n_hidden, n_output,
+                 hidden_transfer='tanh', out_transfer='identity',
+                 loss='squared', pooling=None,
+                 optimizer='rprop',
+                 max_iter=1000,
+                 verbose=False):
+        """Create and return a ``Rnn`` object.
+
+        :param n_inpt: Number of inputs per time step to the network.
+        :param n_hidden: Size of the hidden state.
+        :param n_output: Size of the output of the network.
+        :param hidden_transfer: Transfer function to use for the network. This
+            can either (a) be a string and reference a transfer function from
+            ``breze.component.transfer`` or a function which takes a theano
+            tensor3 and returns a tensor of equal size.
+        :param out_transfer: Output function to use for the network. This
+            can either (a) be a string and reference a transfer function from
+            ``breze.component.transfer`` or a function which takes a theano
+            tensor3 and returns a tensor of equal size.
+        :param loss: Loss which is going to be optimized. This can either be a
+            string and reference a loss function found in
+            ``breze.component.distance`` or a function which takes two theano
+            tensors (one being the output of the network, the other some target)
+            and returns a theano scalar.
+        :param pooling: One of ``sum``, ``mean``, ``prod``, ``min``, ``max`` or
+            ``None``. If not None, the output is pooled over the time dimension,
+            essentially making the network return a tensor2 instead of a
+            tensor3.
+        :param optimizer: Either ``ksd`` referring to KrylovSubspaceDescent or
+            ``rprop``.
+        :param max_iter: Maximum number of optimization iterations to perform.
+        :param verbose: Flag indicating whether to print out information during
+            fitting.
+        """
+        super(BaseLstm, self).__init__(n_inpt, n_hidden, n_output,
+            hidden_transfer, out_transfer, loss, pooling, optimizer, False,
+            max_iter, verbose)
+
+
+class SupervisedLstm(BaseLstm, rnn.SupervisedLstmRecurrentNetwork,
+                     SupervisedBrezeWrapperBase):
+    """Class implementing recurrent neural networks with LSTM cells for
+    supervised learning.
+
+    The class inherits from breze's RecurrentNetwork class and adds several
+    sklearn like methods.
+    """
+    # TODO fix docstring
+
+    def iter_fit(self, X, Z):
+        """Iteratively fit the parameters of the model to the given data with
+        the given error function.
+
+        Each iteration of the learning algorithm is an iteration of the returned
+        iterator. The model is in a valid state after each iteration, so that
+        the optimization can be broken any time by the caller.
+
+        This method does `not` respect the max_iter attribute.
+
+        :param X: A (t, n ,d) array where _t_ is the number of time steps,
+            _n_ is the number of data samples and _d_ is the dimensionality of
+            a data sample at a single time step.
+        :param Z: A (t, n, l) array where _t_ and _n_ are defined as in _X_,
+            but _l_ is the dimensionality of the output sequences at a single
+            time step.
+        """
+
+        f_loss, f_d_loss, f_Hp = self._make_loss_functions()
+
+        args = itertools.repeat(([X, Z], {}))
+        opt = self._make_optimizer(f_loss, f_d_loss, args)
+
+        for i, info in enumerate(opt):
+            yield info
+
+
+class UnsupervisedLstm(BaseLstm, rnn.UnsupervisedLstmRecurrentNetwork,
+                       UnsupervisedBrezeWrapperBase,
+                       TransformBrezeWrapperMixin):
+    """Class implementing recurrent neural networks with LSTM cells for
+    unsupervised learning.
+
+    The class inherits from breze's RecurrentNetwork class and adds several
+    sklearn like methods.
+    """
+    # TODO fix docstring
+    transform_expr_name = 'output'
+
+    def iter_fit(self, X):
+        """Iteratively fit the parameters of the model to the given data with
+        the given error function.
+
+        Each iteration of the learning algorithm is an iteration of the returned
+        iterator. The model is in a valid state after each iteration, so that
+        the optimization can be broken any time by the caller.
+
+        This method does `not` respect the max_iter attribute.
+
+        :param X: A (t, n ,d) array where _t_ is the number of time steps,
+            _n_ is the number of data samples and _d_ is the dimensionality of
+            a data sample at a single time step.
+        """
 
         f_loss, f_d_loss, f_Hp = self._make_loss_functions()
 
--- test/test_rnn.py
+++ test/test_rnn.py
@@ -3,7 +3,9 @@
 import numpy as np
 import theano.tensor as T
 
-from brummlearn.rnn import SupervisedRnn, UnsupervisedRnn
+from brummlearn.rnn import (
+    SupervisedRnn, UnsupervisedRnn,
+    SupervisedLstm, UnsupervisedLstm)
 
 
 def test_srnn_fit():
@@ -24,7 +26,6 @@ def test_srnn_iter_fit():
 
 def test_srnn_predict():
     X = np.random.standard_normal((10, 5, 2))
-    Z = np.random.standard_normal((10, 5, 3))
     rnn = SupervisedRnn(2, 10, 3, max_iter=10)
     rnn.predict(X)
 
@@ -46,4 +47,46 @@ def test_usrnn_iter_fit():
 def test_usrnn_transform():
     X = np.random.standard_normal((10, 5, 2))
     rnn = UnsupervisedRnn(2, 10, 3, loss=lambda x: T.log(x.sum()), max_iter=10)
+    rnn.transform(X)
+
+
+def test_slstm():
+    X = np.random.standard_normal((10, 5, 2))
+    Z = np.random.standard_normal((10, 5, 3))
+    rnn = SupervisedLstm(2, 10, 3, max_iter=10)
+    rnn.fit(X, Z)
+
+
+def test_slstm_iter_fit():
+    X = np.random.standard_normal((10, 5, 2))
+    Z = np.random.standard_normal((10, 5, 3))
+    rnn = SupervisedLstm(2, 10, 3, max_iter=10)
+    for i, info in enumerate(rnn.iter_fit(X, Z)):
+        if i >= 10:
+            break
+
+
+def test_slstm_predict():
+    X = np.random.standard_normal((10, 5, 2))
+    rnn = SupervisedLstm(2, 10, 3, max_iter=10)
+    rnn.predict(X)
+
+
+def test_uslstm_fit():
+    X = np.random.standard_normal((10, 5, 2))
+    rnn = UnsupervisedLstm(2, 10, 3, loss=lambda x: T.log(x.sum()), max_iter=10)
+    rnn.fit(X)
+
+
+def test_uslstm_iter_fit():
+    X = np.random.standard_normal((10, 5, 2))
+    rnn = UnsupervisedLstm(2, 10, 3, loss=lambda x: T.log(x.sum()), max_iter=10)
+    for i, info in enumerate(rnn.iter_fit(X)):
+        if i >= 10:
+            break
+
+
+def test_uslstm_transform():
+    X = np.random.standard_normal((10, 5, 2))
+    rnn = UnsupervisedLstm(2, 10, 3, loss=lambda x: T.log(x.sum()), max_iter=10)
     rnn.transform(X)
