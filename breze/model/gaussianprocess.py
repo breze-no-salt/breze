bfbdd16301a094ad1286440695ffa3fa1bc9d85d
Justin Bayer <bayer.justin@googlemail.com>
2012-11-06 22:50:26 +0100

    several improvements on numerical stability for GPs

--- breze/model/gaussianprocess.py
+++ breze/model/gaussianprocess.py
@@ -12,37 +12,31 @@ from ..util import ParameterSet, Model, lookup
 from ..component import distance
 
 
-def softabs(X):
-    return T.sqrt(X**2 + 1e-8)
-
-
 def linear_kernel(X, X_, length_scales):
-    length_scales = softabs(length_scales)
-    X *= length_scales.dimshuffle('x', 0)
-    X_ *= length_scales.dimshuffle('x', 0)
+    X = X * length_scales.dimshuffle('x', 0)
+    X_ = X_ * length_scales.dimshuffle('x', 0)
     return T.dot(X, X_.T)
 
 
 def matern52_kernel(X, X_, length_scales):
-    length_scales = softabs(length_scales)
     X = X * length_scales.dimshuffle('x', 0)
     X_ = X_ * length_scales.dimshuffle('x', 0)
-    D = distance.distance_matrix(X, X_, 'soft_l1')
-    return (1.0 + T.sqrt(5.) * D + (5. / 3.) * D**2) * T.exp(-T.sqrt(5.) * D)
+    D2 = distance.distance_matrix(X, X_, 'l2')
+    D = T.sqrt(D2)
+    return (1.0 + T.sqrt(5.) * D + (5. / 3.) * D2) * T.exp(-T.sqrt(5.) * D)
 
 
 def rbf_kernel(X, X_, length_scales):
-    length_scales = softabs(length_scales)
     X = X * length_scales.dimshuffle('x', 0)
     X_ = X_ * length_scales.dimshuffle('x', 0)
-    D = distance.distance_matrix(X, X_, 'soft_l1')
-    return T.exp(-D**2)
+    D2 = distance.distance_matrix(X, X_, 'l2')
+    return T.exp(-D2)
 
 
 class GaussianProcess(Model):
 
     minimal_noise = 1e-4
-    maximal_length_scales = 10
+    minimal_length_scale = 1e-8
 
     def __init__(self, n_inpt, kernel='linear'):
         self.n_inpt = n_inpt
@@ -70,18 +64,23 @@ class GaussianProcess(Model):
     @staticmethod
     def make_exprs(inpt, test_inpt, target,
                    length_scales, noise, kernel):
-        noise = T.maximum(softabs(noise), GaussianProcess.minimal_noise)
-        length_scales = T.minimum(softabs(length_scales),
-                                  GaussianProcess.maximal_length_scales)
+
+        # To stay compatible to the prediction api, target will
+        # be a matrix to the outside. But in the following, it's easier
+        # if it is a vector in the inside. We keep a reference to the
+        # matrix anyway, to return it.
+        target_ = target
+        target = target[:, 0]
+        noise = T.exp(noise) + GaussianProcess.minimal_noise
+        length_scales = T.exp(length_scales) + GaussianProcess.minimal_length_scale
 
         kernel_func = globals()['%s_kernel' % kernel]
 
         # For training.
         K = kernel_func(inpt, inpt, length_scales)
 
-        # We add the noise variable, but at least 1e-4 for
-        # numerical stability.
         K += T.identity_like(K) * noise
+
         # Justin: This is an informed choice. I played around a little
         # with various methods (e.g. using cholesky first) and came to
         # the conclusion that this way of doing it was way faster than
@@ -89,32 +88,35 @@ class GaussianProcess(Model):
         psd(K)
         inv_K = minv(K)
 
-        nll = 0.5 * (
-            det(K)
-            + T.dot(T.dot(target.T, inv_K), target).mean()
-            + K.shape[0] * T.log(2 * np.pi))
+        n_samples = K.shape[0]
+        nll = (
+            0.5 * T.dot(T.dot(target.T, inv_K), target)
+            + 0.5 * T.log(det(K) + 1e-8)
+            + 0.5 * n_samples * T.log(2 * np.pi))
 
         # For prediction.
         inference_kernelrows = kernel_func(inpt, test_inpt, length_scales)
         test_K = kernel_func(test_inpt, test_inpt, length_scales)
 
         kTK = T.dot(inference_kernelrows.T, inv_K)
-        output_mean = T.dot(kTK, target)
+        output_mean = T.dot(kTK, target).dimshuffle(0, 'x')
 
         kTKk = T.dot(kTK, inference_kernelrows)
 
         d = test_inpt.shape[0]
         diag_coords = T.arange(d), T.arange(d)
-        output_std = (test_K - kTKk)[diag_coords]
+        output_var = ((test_K - kTKk)[diag_coords]).dimshuffle(0, 'x')
+
         return {
             'inpt': inpt,
             'test_inpt': test_inpt,
-            'target': target,
+            'target': target_,
             'gram_matrix': K,
             'nll': nll,
             'loss': nll,
             'output': output_mean,
-            'output_std': output_std,
+            'output_var': output_var,
+            'inference_kernelrows': inference_kernelrows,
             'test_K': test_K,
             'kTKk': kTKk,
         }
