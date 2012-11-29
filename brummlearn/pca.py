4c65c00bd7b7350c3480f789ab4133dd2a2d961c
Justin Bayer <bayer.justin@googlemail.com>
2012-11-29 16:57:22 +0100

    adapted pca to sklearn api

--- brummlearn/pca.py
+++ brummlearn/pca.py
@@ -9,30 +9,29 @@ import numpy as np
 import scipy.linalg
 
 
-def pca(X, n_components=None, whiten=False):
-    """Apply principal component analysis to a dataset and return the
-    corresponding matrix `w` to extract those components.
+class Pca(object):
 
-    If `whiten` is set, the data set will also be whitened.
+    def __init__(self, n_components=None, whiten=False):
+        """Create a Pca object.
 
-    Items can be projected into this space by ``np.dot(X, w)``, where w is the
-    new frame.
+        :param n_components: Amount of components to keep.
+        :param whiten: Flag that indicates whether the data should have
+            isometric and unit covariance after transforming.
+        """
+        self.n_components = n_components
+        self.whiten = whiten
 
-    :param X: 2d array, where the rows index data points and the columns
-              coordinates of those points.
-    :param n_components: Amount of components to keep.
-    :param whiten: Flag that indicates whether the data should have isometric
-                   and unit covariance.
-    :returns: ``(w, s)`` where ``w`` is the frame of components with highest 
-              variance and ``s`` is the array of eigenvalues which correspond to
-              the explained variance.
-    """
-    if n_components is None:
-        n_components = X.shape[1]
-    cov = scipy.cov(X, rowvar=0)
-    w, s, v = scipy.linalg.svd(cov, full_matrices=False)
-    w = w[:, :n_components]
-    if whiten:
-        s_ = s[:n_components]
-        w = np.dot(w, np.diag(1. / np.sqrt(s_)))
-    return w, s
+    def fit(self, X):
+        n_components = X.shape[1] if self.n_components is None else self.n_components
+        cov = np.cov(X, rowvar=0)
+        w, s, v = scipy.linalg.svd(cov, full_matrices=False)
+        w = w[:, :n_components]
+        if self.whiten:
+            s_ = s[:n_components]
+            w = np.dot(w, np.diag(1. / np.sqrt(s_)))
+
+        self.weights = w
+        self.singular_values = s
+
+    def transform(self, X):
+        return np.dot(X, self.weights)
