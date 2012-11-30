f64730a969744980949bd69d470398bd8207aafd
Justin Bayer <bayer.justin@googlemail.com>
2012-11-30 17:06:16 +0100

    Added denoising auto encoder.

--- brummlearn/autoencoder.py
+++ brummlearn/autoencoder.py
@@ -7,7 +7,8 @@ import numpy as np
 from breze.model.feature import (
     AutoEncoder as _AutoEncoder,
     SparseAutoEncoder as _SparseAutoEncoder,
-    ContractiveAutoEncoder as _ContractiveAutoEncoder)
+    ContractiveAutoEncoder as _ContractiveAutoEncoder,
+    DenoisingAutoEncoder as _DenoisingAutoEncoder)
 from brummlearn.base import (
     UnsupervisedBrezeWrapperBase, TransformBrezeWrapperMixin,
     ReconstructBrezeWrapperMixin)
@@ -163,6 +164,64 @@ class ContractiveAutoEncoder(_ContractiveAutoEncoder,
         super(ContractiveAutoEncoder, self).__init__(
             n_inpt, n_hidden, hidden_transfer, out_transfer,
             reconstruct_loss, c_jacobian,
+            tied_weights)
+        self.batch_size = batch_size
+        self.optimizer = optimizer
+        self.f_transform = None
+        self.f_reconstruct = None
+        self.parameters.data[:] = np.random.standard_normal(
+            self.parameters.data.shape)
+        self.max_iter = max_iter
+        self.verbose = verbose
+
+
+class DenoisingAutoEncoder(_DenoisingAutoEncoder, UnsupervisedBrezeWrapperBase,
+                           TransformBrezeWrapperMixin,
+                           ReconstructBrezeWrapperMixin):
+
+    transform_expr_name = 'hidden'
+
+    def __init__(self, n_inpt, n_hidden, hidden_transfer='sigmoid',
+                 out_transfer='identity', reconstruct_loss='squared',
+                 noise_type='gauss', c_noise=.2,
+                 tied_weights=True, batch_size=None,
+                 optimizer='lbfgs', max_iter=1000, verbose=False):
+        """Create a SparseAutoEncoder object.
+
+        :param n_inpt: Input dimensionality of the data.
+        :param n_hidden: Dimensionality of the hidden feature dimension.
+        :param hidden_transfer: Transfer function to use for the hidden units.
+            Can be a string referring any function found in
+            ``breze.component.transfer`` or a function that given an (n, d)
+            array returns an (n, d) array as theano expressions.
+        :param out_transfer: Output transfer function of the linear auto encoder
+            for calculation of the reconstruction cost.
+        :param reconstruct_loss: Reconstruction part of the loss which is
+            going to be optimized. This can either be a string and reference a
+            loss function found in ``breze.component.distance`` or a function
+            which takes two theano tensors (one being the output of the network,
+            the other some target) and returns a theano scalar.
+        :param noise_type: Specifies the type of noise used, either 'gauss' or
+            'blink'. The former adds Gaussian noise, the latter sets inputs
+            random to zero.
+        :param c_noise: Standard deviation of the noise in case of Gaussian
+            noise, "set to zero" probability in case of blink noise.
+        :param tied_weights: Flag indicating whether to use tied weights.
+        :param batch_size: Number of examples per batch when calculing the loss
+            and its derivatives. None means to use all samples every time.
+        :param optimizer: Can be either a string or a pair. In any case,
+            climin.util.optimizer is used to construct an optimizer. In the case
+            of a string, the string is used as an identifier for the optimizer
+            which is then instantiated with default arguments. If a pair,
+            expected to be (`identifier`, `kwargs`) for more fine control of the
+            optimizer.
+        :param max_iter: Maximum number of optimization iterations to perform.
+        :param verbose: Flag indicating whether to print out information during
+            fitting.
+        """
+        super(DenoisingAutoEncoder, self).__init__(
+            n_inpt, n_hidden, hidden_transfer, out_transfer,
+            reconstruct_loss, noise_type, c_noise,
             tied_weights)
         self.batch_size = batch_size
         self.optimizer = optimizer
