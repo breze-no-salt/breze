03494a9f4640b3fa307e04c3ef73f3d6d581cb2e
Justin Bayer <bayer.justin@googlemail.com>
2013-01-09 11:26:45 +0100

    added module for corrupting random variables

---
+++ breze/component/corrupt.py
@@ -0,0 +1,21 @@
+"""This module contains functionality to corrupt Theano variables
+with noise."""
+
+import theano.tensor as T
+
+
+def gaussian_perturb(x, std, rng=None):
+    """Return a Theano variable which is ``x + e`` where ``e`` is Gaussian noise
+    with a standard deviation of `std`."""
+    if rng is None:
+        rng = T.shared_randomstreams.RandomStreams()
+    return x + rng.normal(size=x.shape, std=std)
+
+
+def mask(x, p, rng=None):
+    """Return a Theano variable which is ``x`` with elements of it set to zero
+    with a probability of ``p``."""
+    if rng is None:
+        rng = T.shared_randomstreams.RandomStreams()
+    this_mask = rng.binomial(size=x.shape, p=p)
+    return x * this_mask
