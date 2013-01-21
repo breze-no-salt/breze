52223c68d0c91f834c2ab6a444b7afc239d2c68d
Justin Bayer <bayer.justin@googlemail.com>
2013-01-21 10:56:40 +0100

    aesthetical

--- brummlearn/utils.py
+++ brummlearn/utils.py
@@ -30,18 +30,18 @@
 import numpy
 
 
-def scale_to_unit_interval(ndar,eps=1e-8):
+def scale_to_unit_interval(ndar, eps=1e-8):
     """Return a copy of ndar with all values scaled between 0 and 1."""
     ndar = ndar.copy()
     ndar -= ndar.min()
-    ndar *= 1.0 / (ndar.max()+eps)
+    ndar *= 1.0 / (ndar.max() + eps)
     return ndar
 
 
-def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
-              scale_rows_to_unit_interval = True, output_pixel_vals = True):
-    """
-    Transform an array with one flattened image per row, into an array in
+def tile_raster_images(X, img_shape, tile_shape, tile_spacing=(0, 0),
+                       scale_rows_to_unit_interval=True,
+                       output_pixel_vals=True):
+    """Transform an array with one flattened image per row, into an array in
     which images are reshaped and layed out like tiles on a floor.
 
     This function is useful for visualizing datasets whose rows are images,
@@ -49,7 +49,7 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
     (such as the first layer of a neural net).
 
     :type X: a 2-D ndarray or a tuple of 4 channels, elements of which can
-    be 2-D ndarrays or None;
+        be 2-D ndarrays or None;
     :param X: a 2-D array in which every row is a flattened image.
 
     :type img_shape: tuple; (height, width)
@@ -59,16 +59,14 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
     :param tile_shape: the number of images to tile (rows, cols)
 
     :param output_pixel_vals: if output should be pixel values (i.e. int8
-    values) or floats
+        values) or floats
 
     :param scale_rows_to_unit_interval: if the values need to be scaled before
-    being plotted to [0,1] or not
-
+        being plotted to [0,1] or not
 
     :returns: array suitable for viewing as an image.
-    (See:`PIL.Image.fromarray`.)
+        (See:`PIL.Image.fromarray`.)
     :rtype: a 2-d array with same dtype as X.
-
     """
 
     assert len(img_shape) == 2
@@ -84,7 +82,7 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
     # out_shape[1] = (img_shape[1]+tile_spacing[1])*tile_shape[1] -
     #                tile_spacing[1]
     out_shape = [(ishp + tsp) * tshp - tsp for ishp, tshp, tsp
-                        in zip(img_shape, tile_shape, tile_spacing)]
+                 in zip(img_shape, tile_shape, tile_spacing)]
 
     if isinstance(X, tuple):
         assert len(X) == 4
@@ -96,9 +94,9 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
 
         #colors default to 0, alpha defaults to 1 (opaque)
         if output_pixel_vals:
-            channel_defaults = [0,0,0,255]
+            channel_defaults = [0, 0, 0, 255]
         else:
-            channel_defaults = [0.,0.,0.,1.]
+            channel_defaults = [0., 0., 0., 1.]
 
         for i in xrange(4):
             if X[i] is None:
@@ -107,12 +105,14 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
                 dt = out_array.dtype
                 if output_pixel_vals:
                     dt = 'uint8'
-                out_array[:,:,i] = numpy.zeros(out_shape,
-                        dtype=dt)+channel_defaults[i]
+                out_array[:, :, i] = (
+                    numpy.zeros(out_shape, dtype=dt) + channel_defaults[i])
             else:
                 # use a recurrent call to compute the channel and store it
                 # in the output
-                out_array[:,:,i] = tile_raster_images(X[i], img_shape, tile_shape, tile_spacing, scale_rows_to_unit_interval, output_pixel_vals)
+                out_array[:, :, i] = tile_raster_images(
+                    X[i], img_shape, tile_shape, tile_spacing,
+                    scale_rows_to_unit_interval, output_pixel_vals)
         return out_array
 
     else:
@@ -126,7 +126,6 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
             dt = 'uint8'
         out_array = numpy.zeros(out_shape, dtype=dt)
 
-
         for tile_row in xrange(tile_shape[0]):
             for tile_col in xrange(tile_shape[1]):
                 if tile_row * tile_shape[1] + tile_col < X.shape[0]:
@@ -134,7 +133,9 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
                         # if we should scale values to be between 0 and 1
                         # do this by calling the `scale_to_unit_interval`
                         # function
-                        this_img = scale_to_unit_interval(X[tile_row * tile_shape[1] + tile_col].reshape(img_shape))
+                        this_img = scale_to_unit_interval(
+                            X[tile_row * tile_shape[1]
+                             + tile_col].reshape(img_shape))
                     else:
                         this_img = X[tile_row * tile_shape[1] + tile_col].reshape(img_shape)
                     # add the slice to the corresponding position in the
@@ -151,10 +152,47 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
 
 
 def one_hot(array, classes):
-    """Given a flat array `array` containing positive integers indicating a class,
-    starting at 0, return a matrix with one row per item in `array` where that
-    element is set to 1, everything else 0."""
+    """Given a flat array `array` containing positive integers indicating a
+    class, starting at 0, return a matrix with one row per item in `array` where
+    that element is set to 1, everything else 0."""
     n = array.shape[0]
     arr = numpy.zeros((n, classes), dtype=numpy.float32)
     arr[xrange(n), array] = 1.
     return arr
+
+
+# From the matpplotlib cookbook at
+# http://www.scipy.org/Cookbook/Matplotlib/HintonDiagrams.
+# Adapte to work on an axis which can be given as an argument.
+
+def _blob(ax, x, y, area, colour):
+    """Draws a square-shaped blob with the given area (< 1) at
+    the given coordinates."""
+    hs = np.sqrt(area) / 2
+    xcorners = np.array([x - hs, x + hs, x + hs, x - hs])
+    ycorners = np.array([y - hs, y - hs, y + hs, y + hs])
+    ax.fill(xcorners, ycorners, colour, edgecolor=colour)
+
+
+def hinton(ax, W, max_weight=None):
+    """
+    Draws a Hinton diagram  for the matrix `W` to axis `ax`.
+    """
+    reenable = False
+
+    height, width = W.shape
+    if not max_weight:
+        max_weight = 2**np.ceil(np.log(np.max(np.abs(W)))/np.log(2))
+
+    ax.fill(np.array([0,width,width,0]),np.array([0,0,height,height]),'gray')
+    ax.axis('off')
+    ax.axis('equal')
+    for x in xrange(width):
+        for y in xrange(height):
+            _x = x+1
+            _y = y+1
+            w = W[y,x]
+            if w > 0:
+                _blob(ax, _x - 0.5, height - _y + 0.5, min(1,w/max_weight),'white')
+            elif w < 0:
+                _blob(ax, _x - 0.5, height - _y + 0.5, min(1,-w/max_weight),'black')
