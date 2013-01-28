83d6375338b18595c790022d9f298ee6d67c77f3
Justin Bayer <bayer.justin@googlemail.com>
2013-01-28 08:43:53 +0100

    SparseCoding available directly from feature package

--- breze/model/feature/__init__.py
+++ breze/model/feature/__init__.py
@@ -1,5 +1,6 @@
 from autoencoder import AutoEncoder, ContractiveAutoEncoder, SparseAutoEncoder, DenoisingAutoEncoder
 from rica import Rica
+from sparsecoding import SparseCoding
 from sparsefiltering import SparseFiltering
 from util import (
     cascade_layers, mlp_from_cascade, feature_func_from_model,
