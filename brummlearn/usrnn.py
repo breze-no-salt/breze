6f76171595f75f1dbab11be1da719225147c785c
Justin Bayer <bayer.justin@googlemail.com>
2013-02-07 10:11:06 +0100

    added sparse filtering rnn

--- brummlearn/usrnn.py
+++ brummlearn/usrnn.py
@@ -1,7 +1,9 @@
 import theano.tensor as T
 
 from .rnn import UnsupervisedRnn
+from breze.model.sequential.rnn import weighted_pooling
 from breze.component.misc import discrete_entropy
+from breze.component import norm
 
 
 def dummy_loss(X):
@@ -45,3 +47,45 @@ class RimRnn(UnsupervisedRnn):
             'l2': l2,
             'loss': neg_mi + self.c_l2 * l2,
         })
+
+
+class SfRnn(UnsupervisedRnn):
+
+    def __init__(self, n_inpt, n_hidden, n_output,
+                 hidden_transfer='tanh',
+                 out_transfer='softabs',
+                 pooling='mean',
+                 leaky_coeffs=None,
+                 optimizer='rprop',
+                 batch_size=None,
+                 max_iter=1000,
+                 verbose=False):
+        super(SfRnn, self).__init__(
+            n_inpt, n_hidden, n_output, hidden_transfer, out_transfer,
+            dummy_loss, pooling, leaky_coeffs, optimizer, batch_size,
+            max_iter, verbose)
+
+    def init_exprs(self):
+        super(SfRnn, self).init_exprs()
+
+        output = self.exprs['output']
+
+        col_normalized = T.sqrt(
+            norm.normalize(output, lambda x: x**2, axis=0) + 1E-8)
+        row_normalized = T.sqrt(
+            norm.normalize(col_normalized, lambda x: x**2, axis=1) + 1E-8)
+
+        loss_rowwise = row_normalized.sum(axis=1)
+        loss = loss_rowwise.mean()
+
+        if self.pooling == 'stochastic':
+            representation = weighted_pooling(self.exprs['unpooled'])
+        else:
+            representation = output
+
+        self.exprs.update({
+            'col_normalized': col_normalized,
+            'row_normalized': row_normalized,
+            'loss_rowwise': loss_rowwise,
+            'representation': representation,
+            'loss': loss})
