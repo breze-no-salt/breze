7fdd4e7376b542dfae3da266b32485e2229db628
Justin Bayer <bayer.justin@googlemail.com>
2013-03-11 13:30:20 +0100

    tidying up gp module some more

--- breze/model/gaussianprocess.py
+++ breze/model/gaussianprocess.py
@@ -46,7 +46,7 @@ class GaussianProcess(Model):
     @staticmethod
     def make_exprs(inpt, test_inpt, target,
                    length_scales, noise, amplitude, kernel):
-
+        exprs = {}
         # To stay compatible to the prediction api, target will
         # be a matrix to the outside. But in the following, it's easier
         # if it is a vector in the inside. We keep a reference to the
@@ -57,17 +57,32 @@ class GaussianProcess(Model):
         length_scales = T.exp(length_scales) + GaussianProcess.minimal_length_scale
         amplitude = T.exp(amplitude) + 1e-4
 
+        # In the case of stationary kernels (those which work on the distances
+        # only) we can save some work by caching the distances. Thus we first
+        # find out if it is a stationary tensor by checking whether the kernel
+        # can be computed by looking at diffs only---this is the case if a
+        # ``XXX_by_diff`` function is available in the kernel modile.
+        # I that is the case, we add the diff expr to the exprs dict, so it can
+        # be exploited by code on the top via a givens directory.
+
+        kernel_by_dist_func = lookup('%s_by_dist' % kernel, kernel_, None)
+        stationary = kernel_by_dist_func is not None
         kernel_func = lookup(kernel, kernel_)
 
-        # For training.
-        K = kernel_func(inpt, inpt, length_scales, amplitude)
+        if stationary:
+            diff = exprs['diff'] = misc.pairwise_diff(inpt, inpt)
+            D2 = exprs['sqrd_dist'] = misc.distance_matrix_by_diff(diff, 'l2')
+            K = kernel_by_dist_func(D2)
+            exprs['D2'] = D2
+        else:
+            K = kernel_func(inpt, inpt, length_scales, amplitude)
 
         K += T.identity_like(K) * noise
 
-        # Justin: This is an informed choice. I played around a little
-        # with various methods (e.g. using cholesky first) and came to
-        # the conclusion that this way of doing it was way faster than
-        # anything else in my case.
+        # This is an informed choice. I played around a little with various
+        # methods (e.g. using cholesky first) and came to the conclusion that
+        # this way of doing it was way faster than explicitly doing a Cholesky
+        # or so.
         psd(K)
         inv_K = minv(K)
 
@@ -77,23 +92,30 @@ class GaussianProcess(Model):
             + 0.5 * T.log(det(K) + 1e-8)
             + 0.5 * n_samples * T.log(2 * np.pi))
 
-        # For prediction.
-        inference_kernelrows = kernel_func(inpt, test_inpt, length_scales,
-                                           amplitude)
+        # We are interested in a loss that is invariant to the number of
+        # samples.
+        nll /= n_samples
 
-        kTK = T.dot(inference_kernelrows.T, inv_K)
+        # Whenever we are working with points not in the training set, the
+        # corresponding expressions are prefixed with test_. Thus test_inpt,
+        # test_K (this is the Kernel matrix of the test inputs only), and
+        # test_kernel (this is the kernel matrix of the training inpt with the
+        # test inpt.
+        test_kernel = kernel_func(inpt, test_inpt, length_scales, amplitude)
+
+        kTK = T.dot(test_kernel.T, inv_K)
         output_mean = T.dot(kTK, target).dimshuffle(0, 'x')
 
-        kTKk = T.dot(kTK, inference_kernelrows)
+        kTKk = T.dot(kTK, test_kernel)
 
         chol_inv_K = cholesky(inv_K)
 
-        diag_kTKk = (T.dot(chol_inv_K.T, inference_kernelrows)**2).sum(axis=0)
+        diag_kTKk = (T.dot(chol_inv_K.T, test_kernel) ** 2).sum(axis=0)
         test_K = kernel_func(test_inpt, test_inpt, length_scales, amplitude,
                              diag=True)
         output_var = ((test_K - diag_kTKk)).dimshuffle(0, 'x')
 
-        return {
+        exprs.update({
             'inpt': inpt,
             'test_inpt': test_inpt,
             'target': target_,
@@ -104,7 +126,8 @@ class GaussianProcess(Model):
             'loss': nll,
             'output': output_mean,
             'output_var': output_var,
-            'inference_kernelrows': inference_kernelrows,
+            'test_kernel': test_kernel,
             'test_K': test_K,
             'kTKk': kTKk,
-        }
+        })
+        return exprs
