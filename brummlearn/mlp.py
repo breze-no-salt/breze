fe2b01ea87c90e99bcd67321f9b8e4e7c78afea1
Justin Bayer <bayer.justin@googlemail.com>
2013-03-12 11:27:01 +0100

    tuned mop docs a little towards numpydoc

--- brummlearn/mlp.py
+++ brummlearn/mlp.py
@@ -19,41 +19,56 @@ from brummlearn.base import SupervisedBrezeWrapperBase
 
 
 class Mlp(MultiLayerPerceptron, SupervisedBrezeWrapperBase):
+    """Multilayer perceptron class.
+
+    This implementation uses a stack of affine mappings with a subsequent
+    non linearity each.
+
+    Parameters
+    ----------
+
+    n_inpt : integer
+        Dimensionality of a single input.
+
+    n_hiddens : list of integers
+        List of ``k`` integers, where ``k`` is thenumber of layers. Each gives
+        the size of the corresponding layer.
+
+    n_output : integer
+        Dimensionality of a single output.
+
+    hidden_transfers : list, each item either string or function
+        Transfer functions for each of the layers. Can be either a string which
+        is then used to look up a transfer function in
+        ``breze.component.transfer`` or a function that given a Theano tensor
+        returns a tensor of the same shape.
+
+    out_transfer : string or function
+        Either a string to look up a function in ``breze.component.transfer`` or
+        a function that given a Theano tensor returns a tensor of the same
+        shape.
+
+    optimizer : string, pair
+        Argument is passed to ``climin.util.optimizer`` to construct an
+        optimizer.
+
+    batch_size : integer, None
+        Number of examples per batch when calculting the loss
+        and its derivatives. None means to use all samples every time.
+
+    max_iter : int
+        Maximum number of optimization iterations to perform. Only respected
+        during``.fit()``, not ``.iter_fit()``.
+
+    verbose : boolean
+        Flag indicating whether to print out information during fitting.
+    """
 
     def __init__(self, n_inpt, n_hiddens, n_output,
                  hidden_transfers, out_transfer, loss,
                  optimizer='lbfgs',
                  batch_size=None,
                  max_iter=1000, verbose=False):
-        """Create an Mlp object.
-
-        This implementation uses a stack of affine mappings with a subsequent
-        non linearity.
-
-        :param n_inpt: Dimensionality of a single input.
-        :param n_hiddens: List of integers, where each integer specifies the
-            size of that layer.
-        :param n_output: Dimensionality of a single output.
-        :param hidden_transfers: List of transfer functions. A transfer function
-            is either a string pointing to a function in
-            ``breze.component.transfer`` or a function taking a theano 2D tensor
-            and returning a tensor of the same shape.
-        :param out_transfers: Output transfer function. A transfer function
-            is either a string pointing to a function in
-            ``breze.component.transfer`` or a function taking a theano 2D tensor
-            and returning a tensor of the same shape.
-        :param optimizer: Can be either a string or a pair. In any case,
-            climin.util.optimizer is used to construct an optimizer. In the case
-            of a string, the string is used as an identifier for the optimizer
-            which is then instantiated with default arguments. If a pair,
-            expected to be (`identifier`, `kwargs`) for more fine control of the
-            optimizer.
-        :param batch_size: Number of examples per batch when calculting the loss
-            and its derivatives. None means to use all samples every time.
-        :param max_iter: Maximum number of optimization iterations to perform.
-        :param verbose: Flag indicating whether to print out information during
-            fitting.
-        """
         super(Mlp, self).__init__(
             n_inpt, n_hiddens, n_output, hidden_transfers, out_transfer,
             loss)
