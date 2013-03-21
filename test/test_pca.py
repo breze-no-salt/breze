5f37906df2a8060904d8ca747d0037fe80ace539
Justin Bayer <bayer.justin@googlemail.com>
2013-03-21 12:23:37 +0100

    one of the pca tests was not run

--- test/test_pca.py
+++ test/test_pca.py
@@ -1,7 +1,6 @@
 # coding: utf-8 -*-
 
 import numpy as np
-import scipy.linalg
 
 from base import roughly
 
@@ -15,7 +14,7 @@ def test_pca():
     X = np.dot(l, expand)
     X += np.random.normal(0, 1E-6, X.shape)
 
-    pca  = Pca(1)
+    pca = Pca(1)
     pca.fit(X)
 
     desired = np.array([[-0.9015], [-0.4327]])
@@ -23,16 +22,16 @@ def test_pca():
     assert roughly(pca.weights, desired, 1E-3)
 
 
-def test_pca():
+def test_pca_white():
     l = np.arange(0,  1, 0.001).reshape((1000, 1))
     expand = np.array([2.5, 1.2]).reshape((1, 2))
 
     X = np.dot(l, expand)
     X += np.random.normal(0, 1E-6, X.shape)
 
-    pca  = Pca(2, whiten=True)
+    pca = Pca(2, whiten=True)
     pca.fit(X)
-    w, s  = pca.weights, pca.singular_values
+    w, s = pca.weights, pca.singular_values
 
     X = pca.transform(X)
     assert roughly(np.cov(X, rowvar=0), np.eye(2), 1E-2), 'covariance not white'
