beaabe5a945a39158a0785f96b681038c12c294c
Justin Bayer <bayer.justin@googlemail.com>
2013-05-08 20:17:40 +0200

    Added mean var softmax via sampling.

--- breze/component/meanvartransfer.py
+++ breze/component/meanvartransfer.py
@@ -2,9 +2,10 @@
 
 
 import numpy as np
+
 import theano.tensor as T
-
-
+import theano.tensor.extra_ops
+from theano.tensor.shared_randomstreams import RandomStreams
 
 
 def normal_pdf(x, location=0, scale=1):
@@ -34,3 +35,27 @@ def rectifier(mean, var):
 
 def identity(mean, var):
     return mean, var
+
+
+def sampling_softmax(axis=1, rng=None):
+    if rng is None:
+        rng = RandomStreams()
+
+    def inner(mean, var):
+        # Generate samples of the distribution.
+        samples = rng.normal(size=mean.shape)
+        samples = samples * T.sqrt(var) + mean
+
+        # Softmax them.
+        # Subtract minimum for numerical stability.
+        samples -= samples.min(axis=axis).dimshuffle(0, 'x')
+        exped = T.exp(samples)
+        normalizer = exped.sum(axis=axis)
+        if axis == 1:
+            result = exped / normalizer.dimshuffle(0, 'x')
+        if axis == 2:
+            result = exped / normalizer.dimshuffle(0, 1, 'x')
+
+        return result, T.zeros_like(var)
+
+    return inner
