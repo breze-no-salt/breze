9c84da7e03931f910691331fb570145de83bc5a6
Justin Bayer <bayer.justin@googlemail.com>
2013-08-12 14:15:53 +0200

    Added simple tests for fast dropout.

--- test/test_mlp.py
+++ test/test_mlp.py
@@ -2,7 +2,9 @@
 
 import numpy as np
 
-from brummlearn.mlp import Mlp
+from brummlearn.mlp import Mlp, FastDropoutNetwork
+
+from breze.component.loss import squared
 
 
 def test_mlp_fit():
@@ -23,6 +25,33 @@ def test_mlp_iter_fit():
 
 def test_mlp_predict():
     X = np.random.standard_normal((10, 2))
-    Z = np.random.standard_normal((10, 1))
     mlp = Mlp(2, [10], 1, ['tanh'], 'identity', 'squared', max_iter=10)
+    mlp.predict(X)
+
+
+def test_fd_fit():
+    X = np.random.standard_normal((10, 2))
+    Z = np.random.standard_normal((10, 1))
+    loss = lambda target, prediction: squared(target, prediction[:, :target.shape[1]])
+    mlp = FastDropoutNetwork(
+        2, [10], 1, ['rectifier'], 'identity', loss, max_iter=10)
+    mlp.fit(X, Z)
+
+
+def test_fd_iter_fit():
+    X = np.random.standard_normal((10, 2))
+    Z = np.random.standard_normal((10, 1))
+    loss = lambda target, prediction: squared(target, prediction[:, :target.shape[1]])
+    mlp = FastDropoutNetwork(
+        2, [10], 1, ['rectifier'], 'identity', loss, max_iter=10)
+    for i, info in enumerate(mlp.iter_fit(X, Z)):
+        if i >= 10:
+            break
+
+
+def test_fd_predict():
+    X = np.random.standard_normal((10, 2))
+    loss = lambda target, prediction: squared(target, prediction[:, :target.shape[1]])
+    mlp = FastDropoutNetwork(
+        2, [10], 1, ['rectifier'], 'identity', loss, max_iter=10)
     mlp.predict(X)
