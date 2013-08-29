4d46e97e1c1c0364aa826a41e12bcb742410c5df
Justin Bayer <bayer.justin@googlemail.com>
2013-08-29 15:46:23 +0200

    Made sampling_softmax for varprop "right": - use theano softmax - make sure that no numerical instabilities emerge

--- breze/component/varprop/transfer.py
+++ breze/component/varprop/transfer.py
@@ -4,6 +4,7 @@ import numpy as np
 
 import theano.tensor as T
 import theano.tensor.extra_ops
+from theano.tensor.nnet import softmax
 from theano.tensor.shared_randomstreams import RandomStreams
 
 from breze.component.distributions import normal
@@ -32,28 +33,29 @@ def rectifier(mean, var):
     return mean_, var_
 
 
-def sampling_softmax(axis=1, rng=None):
+def make_sampling_softmax(axis=1, rng=None):
     if rng is None:
         rng = RandomStreams()
 
     def inner(mean, var):
         # Generate samples of the distribution.
         samples = rng.normal(size=mean.shape)
-        samples = samples * T.sqrt(var) + mean
+        std = T.sqrt(var)
+        samples = samples * std + mean
 
-        # Softmax them.
-        # Subtract minimum for numerical stability.
-        samples -= samples.min(axis=axis).dimshuffle(0, 'x')
-        exped = T.exp(samples)
-        normalizer = exped.sum(axis=axis) + epsilon
         if axis == 1:
-            result = exped / normalizer.dimshuffle(0, 'x')
+            result = softmax(samples) # XXX
+            result.name = 'susp1'
         if axis == 2:
-            result = exped / normalizer.dimshuffle(0, 1, 'x')
+            samples_flat = samples.reshape((samples.shape[0] * samples.shape[1], samples.shape[2]))
+            result_flat = softmax(samples_flat)
+            result = result.reshape(samples.shape)
 
         return result, T.zeros_like(var)
 
     return inner
+
+sampling_softmax = make_sampling_softmax()
 
 
 def sigmoid(mean, var):
