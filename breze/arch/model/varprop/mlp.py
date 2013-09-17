0ab665250e7da333ffa42478cdf4eae6f2edca47
Justin Bayer <bayer.justin@googlemail.com>
2013-09-17 13:10:31 +0200

    added way to project variables into a l2 ball

--- breze/arch/model/varprop/mlp.py
+++ breze/arch/model/varprop/mlp.py
@@ -17,7 +17,8 @@ def mean_var_forward(in_mean, in_var, weights, bias, variance_bias_sqrt,
 
     dropout_var = p_dropout * (1 - p_dropout)
     out_in_var = (T.dot(in_mean ** 2, weights ** 2) * dropout_var
-                  + T.dot(in_var, weights ** 2) * p_keep)
+                  + T.dot(in_var, weights ** 2) * p_keep
+                  + T.dot(in_var, weights ** 2) * dropout_var)
     out_in_var *= variance_bias_sqrt ** 2
     out_mean, out_var = f(out_in_mean, out_in_var)
     return out_in_mean, out_in_var, out_mean, out_var
--- tests/arch/test_component_misc.py
+++ tests/arch/test_component_misc.py
@@ -5,7 +5,7 @@ import theano
 import theano.tensor as T
 import numpy as np
 
-from breze.arch.component.misc import distance_matrix
+from breze.arch.component.misc import distance_matrix, project_into_l2_ball
 from tools import roughly
 
 
@@ -20,3 +20,37 @@ def test_distance_matrix():
     assert correct, 'distance matrix not working right'
 
 
+def test_project_into_l2_ball_single():
+    x = T.vector()
+    x_projected = project_into_l2_ball(x, 1)
+    f = theano.function([x], x_projected)
+
+    x = np.array([.1, .1, .1])
+    y = f(x)
+    assert np.allclose(np.array([.1, .1, .1]), y)
+
+    x = np.array([2, 1, 1])
+    y = f(x)
+    assert np.allclose(np.array([0.81649658, 0.40824829, 0.40824829]), y)
+
+    x = np.array([0, 1, 0])
+    y = f(x)
+    assert np.allclose(np.array([0., 1, 0]), y)
+
+
+def test_project_into_l2_ball_batch():
+    x = T.matrix()
+    x_projected = project_into_l2_ball(x, 1)
+    f = theano.function([x], x_projected)
+
+    x = np.array([[.1, .1, .1],
+                  [0, 1, 0]])
+    y = f(x)
+
+    desired = np.array([
+        [.1, .1, .1],
+        [0., 1, 0],
+    ])
+
+    print y
+    assert np.allclose(desired, y)
