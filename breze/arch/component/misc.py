9d7f69a3ada3529d6896bed61bfd85d0ca24fd1b
Justin Bayer <bayer.justin@googlemail.com>
2013-11-06 16:18:15 +0100

    added function to project theano variables into the l2 ball

--- breze/arch/component/misc.py
+++ breze/arch/component/misc.py
@@ -29,3 +29,18 @@ def distance_matrix_by_diff(diff, norm=norm_.l2):
 def discrete_entropy(X, axis=None):
     X = T.minimum(1, X + 1e-8)
     return -(X * T.log(X)).sum(axis=axis)
+
+
+def project_into_l2_ball(x, radius):
+    # Distinguish whether we are given a single or many vectors to work upon.
+    batch = x.ndim == 2
+    if not batch:
+        x = T.shape_padleft(x)
+
+    lengths = T.sqrt((x ** 2).sum(axis=1)).dimshuffle(0, 'x')
+    x = T.switch(lengths > radius, x / lengths * radius, x)
+
+    if not batch:
+        x = x[0]
+
+    return x
