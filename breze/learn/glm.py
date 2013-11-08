3df287b321b7eed2640e94b29679dcb704329c6a
Justin Bayer <bayer.justin@googlemail.com>
2013-11-08 15:00:03 +0100

    fix issue #9

--- breze/learn/glm.py
+++ breze/learn/glm.py
@@ -9,6 +9,7 @@ from breze.learn.base import SupervisedBrezeWrapperBase
 
 
 class GeneralizedLinearModel(Linear, SupervisedBrezeWrapperBase):
+    """Class to represent a linear model."""
 
     def __init__(self, n_inpt, n_output,
                  out_transfer='identity', loss='squared',
@@ -16,25 +17,42 @@ class GeneralizedLinearModel(Linear, SupervisedBrezeWrapperBase):
                  max_iter=1000, verbose=False):
         """Create a GeneralizedLinearModel object.
 
-        :param n_inpt: Input dimensionality of a single input.
-        :param n_output: Input dimensionality of a single input.
-        :param out_transfer: Either a string pointing to a function in
+        Parameters
+        ---------
+
+        n_inpt : integer
+            Input dimensionality of a single input.
+
+        n_output : integer
+            Input dimensionality of a single input.
+
+        out_transfer : strong or function
+            Either a string pointing to a function in
             ``breze.arch.component.transfer`` or a function taking a theano 2D
             tensor and returning a tensor of the same shape.
-        :param loss: Either a string pointing to a function in
-            ``breze.arch.component.distance`` or a function taking a theano 2D
+
+        loss : string or function
+            Either a string pointing to a function in
+            ``breze.arch.component.loss`` or a function taking a theano 2D
             tensor and returning a Theano scalar.
-        :param optimizer: Can be either a string or a pair. In any case,
-            climin.util.optimizer is used to construct an optimizer. In the case
-            of a string, the string is used as an identifier for the optimizer
-            which is then instantiated with default arguments. If a pair,
-            expected to be (`identifier`, `kwargs`) for more fine control of the
-            optimizer.
-        :param batch_size: Number of examples per batch when calculing the loss
-            and its derivatives. None means to use all samples every time.
-        :param max_iter: Maximum number of optimization iterations to perform.
-        :param verbose: Flag indicating whether to print out information during
-            fitting.
+
+        optimizer : string or pair
+            Can be either a string or a pair. In any case,
+            ``climin.util.optimizer`` is used to construct an optimizer. In the
+            case of a string, the string is used as an identifier for the
+            optimizer which is then instantiated with default arguments. If a
+            pair, expected to be ``(identifier, kwargs)`` for more fine control
+            of the optimizer.
+
+        batch_size : integer
+            Number of examples per batch when calculing the loss and its
+            derivatives. None means to use all samples every time.
+
+        max_iter : integer
+            Maximum number of optimization iterations to perform.
+
+        verbose : boolean
+            Flag indicating whether to print out information during fitting.
         """
         super(GeneralizedLinearModel, self).__init__(
             n_inpt, n_output, out_transfer, loss)
