9c06375bbfd382de873105c8b4a6a25c3311c2be
Justin Bayer <bayer.justin@googlemail.com>
2014-01-10 14:43:27 +0100

    revamped rica and added notebook

--- tests/learn/test_rica.py
+++ tests/learn/test_rica.py
@@ -7,7 +7,7 @@ from breze.learn.rica import Rica
 
 def test_rica_fit():
     X = np.random.standard_normal((10, 2))
-    rica = Rica(2, 10, feature_transfer='softabs',
+    rica = Rica(2, 10, code_transfer='softabs',
                 hidden_transfer='identity', loss='squared',
                 c_ica=0.5, max_iter=10)
     rica.fit(X)
@@ -15,7 +15,7 @@ def test_rica_fit():
 
 def test_rica_iter_fit():
     X = np.random.standard_normal((10, 2))
-    rica = Rica(2, 10, feature_transfer='softabs',
+    rica = Rica(2, 10, code_transfer='softabs',
                 hidden_transfer='identity', loss='squared',
                 c_ica=0.5, max_iter=10)
     for i, info in enumerate(rica.iter_fit(X)):
@@ -25,7 +25,7 @@ def test_rica_iter_fit():
 
 def test_rica_transform():
     X = np.random.standard_normal((10, 2))
-    rica = Rica(2, 10, feature_transfer='softabs',
+    rica = Rica(2, 10, code_transfer='softabs',
                 hidden_transfer='identity', loss='squared',
                 c_ica=0.5, max_iter=10)
     rica.transform(X)
@@ -33,7 +33,7 @@ def test_rica_transform():
 
 def test_rica_reconstruct():
     X = np.random.standard_normal((10, 2))
-    rica = Rica(2, 10, feature_transfer='softabs',
+    rica = Rica(2, 10, code_transfer='softabs',
                 hidden_transfer='identity', loss='squared',
                 c_ica=0.5, max_iter=10)
     rica.reconstruct(X)
