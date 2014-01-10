e783abf086e9cc5739fd368cda04772276c4aa0f
Justin Bayer <bayer.justin@googlemail.com>
2014-01-10 12:24:52 +0100

    Sparse filtering revamped, added notebook for mnist.

--- breze/arch/model/feature/__init__.py
+++ breze/arch/model/feature/__init__.py
@@ -1,7 +1,1 @@
-from rica import Rica
-from sparsecoding import SparseCoding
-from sparsefiltering import SparseFiltering
-from util import (
-    cascade_layers, mlp_from_cascade, feature_func_from_model,
-    get_affine_parameters)
-from rbm import RestrictedBoltzmannMachine
+
