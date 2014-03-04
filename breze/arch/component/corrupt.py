06d8514c612cbf22f4cadc360aaa557f15493dda
Justin Bayer <bayer.justin@googlemail.com>
2014-03-04 13:03:07 +0100

    added more docs

--- breze/arch/component/corrupt.py
+++ breze/arch/component/corrupt.py
@@ -4,21 +4,66 @@ with noise."""
 import theano
 import theano.tensor as T
 
+# TODO better documentations
+# TODO add saltnpepper
 
-def gaussian_perturb(x, std, rng=None):
-    """Return a Theano variable which is ``x + e`` where ``e`` is Gaussian noise
-    with a standard deviation of `std`."""
+
+def gaussian_perturb(arr, std, rng=None):
+    """Return a Theano variable which is perturbed by additive zero-centred
+    Gaussian noise with standard deviation ``std``.
+
+    Parameters
+    ----------
+
+    arr : Theano variable
+        Array of some shape ``n``.
+
+    std : float or scalar Theano variable
+        Standard deviation of the Gaussian noise.
+
+    rng : Theano random number generator, optional [default: None]
+        Generator to draw random numbers from. If None, rng will be
+        instantiated on the spot.
+
+
+    Returns
+    -------
+
+    res : Theano variable
+        Of shape ``n``.
+    """
+
     if rng is None:
         rng = T.shared_randomstreams.RandomStreams()
-    noise = rng.normal(size=x.shape, std=std)
+    noise = rng.normal(size=arr.shape, std=std)
     noise = T.cast(noise, theano.config.floatX)
-    return x + noise
+    return arr + noise
 
 
-def mask(x, p, rng=None):
-    """Return a Theano variable which is ``x`` with elements of it set to zero
-    with a probability of ``p``."""
+def mask(arr, p, rng=None):
+    """Return a Theano variable which is with elements of it set to zero with
+    probability ``p``.
+
+    Parameters
+    ----------
+
+    arr : Theano variable
+        Array of some shape ``n``.
+
+    p : float or scalar Theano variable
+        Probability that a unit is set to zero.
+
+    rng : Theano random number generator, optional [default: None]
+        Generator to draw random numbers from. If None, rng will be
+        instantiated on the spot.
+
+    Returns
+    -------
+
+    res : Theano variable
+        Of shape ``n``.
+    """
     if rng is None:
         rng = T.shared_randomstreams.RandomStreams()
-    this_mask = T.cast(rng.binomial(size=x.shape, p=p), theano.config.floatX)
-    return x * this_mask
+    this_mask = T.cast(rng.binomial(size=arr.shape, p=p), theano.config.floatX)
+    return arr * this_mask
