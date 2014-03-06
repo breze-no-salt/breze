a21431c00cb9fef32147b31670792474f8e25ee2
Justin Bayer <bayer.justin@googlemail.com>
2014-03-06 15:23:08 +0100

    tidying up

--- breze/arch/model/feature/rica.py
+++ breze/arch/model/feature/rica.py
@@ -1,14 +1,37 @@
 # -*- coding: utf-8 -*-
 
 
-import theano.tensor as T
+from ...util import lookup, get_named_variables
+from ...component import transfer as _transfer
 
-from ...util import ParameterSet, Model, lookup
-from ...component import transfer as _transfer, loss as loss_
-from breze.arch.util import ParameterSet, Model, lookup, get_named_variables
+
+# TODO rename to ``loss``
+# TODO add prefix
 
 
 def ica_loss(code, transfer):
+    """Return the ica loss of a code ``code`` given a density function.
+
+    Parameters
+    ----------
+
+    code : Theano variable
+        Array of shape ``(n, d)``, where ``n`` is then number of samples and
+        ``d`` the dimensionality.
+
+    transfer : string or function
+        Density function. Either a function callable that returns a density for
+        each entry of its argument or a string pointing at a function in
+        ``breze.arch.component.transfer``.
+
+    Returns
+    -------
+
+    res : dict
+        Dictionary containing the loss coordinate wise, sample wise and
+        completely. Corresponding keys are ``ica_loss_coord_wise``,
+        ``ica_loss_sample_wise`` and ``ica_loss``.
+    """
     f_transfer= lookup(transfer, _transfer)
     ica_loss_coord_wise = f_transfer(code)
     ica_loss_sample_wise = ica_loss_coord_wise.sum(axis=1)
