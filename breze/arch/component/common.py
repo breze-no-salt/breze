16ccf60546e942c502e05123a648998e880ff480
Justin Bayer <bayer.justin@googlemail.com>
2014-03-07 12:06:00 +0100

    tidying

--- breze/arch/component/common.py
+++ breze/arch/component/common.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-import theano
-import theano.tensor as T
-
 import loss as loss_
 from ..util import lookup, get_named_variables
 
@@ -13,6 +10,37 @@ from ..util import lookup, get_named_variables
 
 
 def supervised_loss(target, prediction, loss, coord_axis=1):
+    """Return a dictionary populated with several expressions for a supervised
+    loss and corresponding targets and predictions.
+
+    Parameters
+    ----------
+
+    target : Theano variable
+        Array representing the target variables.
+
+    prediction : Theano variable
+        Array representing the predictions.
+
+    loss : callable or string
+        If a string, should index a member of :mod:`breze.arch.component.loss`.
+        If a callable, has to be a of the form described in
+        :mod:`breze.arch.component.loss`.
+
+    coord_axis : integer, optional [default: 1]
+        Axis aong which the coordinates of single sample are stored. I.e. not
+        the sample axis or some spatial axis.
+
+    Example
+    -------
+
+    >>> import theano.tensor as T
+    >>> prediction, target = T.matrix('prediction'), T.matrix('target')
+    >>> from breze.arch.component.loss import squared
+    >>> loss_dict = supervised_loss(target, prediction, squared)
+    >>> loss_dict
+    {'loss': Elemwise{true_div,no_inplace}.0, 'loss_coord_wise': Elemwise{pow,no_inplace}.0, 'loss_sample_wise': Sum{1}.0, 'target': target, 'prediction': prediction}
+    """
     f_loss = lookup(loss, loss_)
     loss_coord_wise = f_loss(target, prediction)
     loss_sample_wise = loss_coord_wise.sum(axis=coord_axis)
@@ -21,9 +49,42 @@ def supervised_loss(target, prediction, loss, coord_axis=1):
     return get_named_variables(locals())
 
 
-def unsupervised_loss(prediction, loss, coord_axis=1):
+def unsupervised_loss(output, loss, coord_axis=1):
+    """Return a dictionary populated with several expressions for a
+    unsupervised loss and corresponding output.
+
+    Parameters
+    ----------
+
+    target : Theano variable
+        Array representing the target variables.
+
+    prediction : Theano variable
+        Array representing the predictions.
+
+    loss : callable or string
+        If a string, should index a member of :mod:`breze.arch.component.loss`.
+        If a callable, has to be a of the form described in
+        :mod:`breze.arch.component.loss`.
+
+    coord_axis : integer, optional [default: 1]
+        Axis aong which the coordinates of single sample are stored. I.e. not
+        the sample axis or some spatial axis.
+
+    Example
+    -------
+
+    >>> import theano.tensor as T
+    >>> output = T.matrix('output')
+    >>> my_loss = lambda x: abs(x)
+    >>> loss_dict = unsupervised_loss(output, my_loss)
+    >>> loss_dict
+    {'loss': Elemwise{true_div,no_inplace}.0, 'output': output, 'loss_sample_wise': Sum{1}.0, 'loss_coord_wise': Elemwise{abs_,no_inplace}.0}
+
+
+    """
     f_loss = lookup(loss, loss_)
-    loss_coord_wise = f_loss(prediction)
+    loss_coord_wise = f_loss(output)
     loss_sample_wise = loss_coord_wise.sum(axis=coord_axis)
     loss = loss_sample_wise.mean()
 
