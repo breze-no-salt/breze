677691ec23b6f313a6905d30813a2a5e7823f744
Justin Bayer <bayer.justin@googlemail.com>
2014-09-23 14:29:29 +0200

    FIX: unnamed gpu variables made function compilation crash

--- breze/arch/util.py
+++ breze/arch/util.py
@@ -596,7 +596,8 @@ class Model(object):
                 gpu_var = self.gpu_variable_subs[var]
             else:
                 gpu_var = cpu_tensor_to_gpu(var)
-                gpu_var.name = var.name + '-for-gpu'
+                if isinstance(getattr(var, 'name', None), str):
+                    gpu_var.name = var.name + '-for-gpu'
                 self.gpu_variable_subs[var] = gpu_var
             gpu_var_flat.append(gpu_var)
         gpu_variables = unflatten(variables, gpu_var_flat)
