5f918749a64f1537a6567719144e1092d04bc228
Justin Bayer <bayer.justin@googlemail.com>
2015-05-06 11:55:46 +0200

    pep8

--- breze/arch/construct/sgvb.py
+++ breze/arch/construct/sgvb.py
@@ -8,7 +8,9 @@ from breze.arch.construct.base import Layer
 
 class VariationalAutoEncoder(Layer):
 
-    def __init__(self, inpt, n_inpt, n_latent, n_output, assumptions, recog_class, gen_class, condition=None, declare=None, name=None):
+    def __init__(self, inpt, n_inpt, n_latent, n_output, assumptions,
+                 recog_class, gen_class, condition=None,
+                 declare=None, name=None):
         self.inpt = inpt
         self.n_inpt = n_inpt
         self.n_latent = n_latent
@@ -18,22 +20,23 @@ class VariationalAutoEncoder(Layer):
         self.gen_class = gen_class
         self.condition = condition
         self.transfer = _transfer
-        super(VariationalAutoEncoder, self).__init__(declare=declare, name=name)
+        super(VariationalAutoEncoder, self).__init__(
+            declare=declare, name=name)
 
     def _forward(self):
         rng = T.shared_randomstreams.RandomStreams()
 
-        # recognition model + sampling
-        self.recog = self.recog_class(self.inpt,self.declare)
+        # Recognition model + sampling
+        self.recog = self.recog_class(self.inpt, self.declare)
         self.latent = self.recog.output
-        self.sample = self.assumptions.sample_latents(self.latent,rng)
+        self.sample = self.assumptions.sample_latents(self.latent, rng)
 
         if self.condition is None:
-            gen_inpt = self.sample 
+            gen_inpt = self.sample
         else:
-            gen_inpt = T.concatenate([self.sample, self.condition],
-                                 axis=latent.ndim - 1)
+            gen_inpt = T.concatenate(
+                [self.sample, self.condition], axis=self.latent.ndim - 1)
 
-        #generative model
-        self.gen = self.gen_class(gen_inpt,self.declare)
+        # Generative model
+        self.gen = self.gen_class(gen_inpt, self.declare)
         self.output = self.gen.output
