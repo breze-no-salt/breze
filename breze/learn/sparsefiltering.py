342d351ef81d0f2197be8336611ce849825802ca
Max Fiedler <fiedler.max@gmail.com>
2015-06-16 11:11:45 +0200

    Sparse Filtering adjusted to new Breze setup
    - code in \model no longer used
    - \learn\sparsefiltering.py adjusted to work with new construct package
    - sparse_filtering_loss added to \component\loss.py

--- breze/learn/sparsefiltering.py
+++ breze/learn/sparsefiltering.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 
 """Sparse Filtering.
-
 As introduced in
-
     Sparse Filtering
     Jiquan Ngiam, Pangwei Koh, Zhenghao Chen, Sonia Bhaskar and Andrew Y. Ng.
     In NIPS*2011.
-
 """
 
 import numpy as np
 import theano
 import theano.tensor as T
 
-from breze.arch.model.feature import sparsefiltering
-from breze.arch.model import linear
-from breze.arch.util import ParameterSet, Model
+from breze.arch.component.loss import sparse_filtering_loss
+from breze.arch.util import ParameterSet
 from breze.learn.base import (
-    UnsupervisedBrezeWrapperBase, TransformBrezeWrapperMixin)
+    UnsupervisedModel, TransformBrezeWrapperMixin,
+    ReconstructBrezeWrapperMixin)
+
+from breze.arch.construct import neural, simple
+from breze.learn.utils import theano_floatx
+
+theano.config.compute_test_value = 'raise'
 
 
-class SparseFiltering(Model, UnsupervisedBrezeWrapperBase,
+class SparseFiltering(UnsupervisedModel,
                       TransformBrezeWrapperMixin):
 
     transform_expr_name = 'output'
@@ -29,24 +31,18 @@ class SparseFiltering(Model, UnsupervisedBrezeWrapperBase,
     def __init__(self, n_inpt, n_output, feature_transfer='softabs',
                  optimizer='lbfgs', max_iter=1000, verbose=False):
         """Create a SparseFiltering object.
-
         Parameters
         ----------
-
         n_inpt : int
             Input dimensionality of the data.
-
         n_output: int
             Dimensionality of the hidden feature dimension.
-
         feature_transfer : string or callable
             Transfer function to use. If a string referring any function found
             in ``breze.arch.component.transfer`` or a function that given an
             (n, d) array returns an (n, d) array as theano expressions.
-
         max_iter : int
             Maximum number of optimization iterations to perform.
-
         verbose : bool
             Flag indicating whether to print out information during fitting.
         """
@@ -58,22 +54,25 @@ class SparseFiltering(Model, UnsupervisedBrezeWrapperBase,
         self.max_iter = max_iter
         self.verbose = verbose
 
-        super(SparseFiltering, self).__init__()
+        self._init_exprs()
 
-    def _init_pars(self):
-        spec = sparsefiltering.parameters(self.n_inpt, self.n_output)
-        self.parameters = ParameterSet(**spec)
-        self.parameters.data[:] = np.random.standard_normal(
-            self.parameters.data.shape).astype(theano.config.floatX)
 
     def _init_exprs(self):
-        self.exprs = {
-            'inpt': T.matrix('inpt'),
-        }
-        self.exprs['inpt'].tag.test_value = np.zeros(
-            (10, self.n_inpt)).astype(theano.config.floatX)
-        P = self.parameters
+        inpt = T.matrix('inpt')
+        inpt.tag.test_value, = theano_floatx(np.ones((3, self.n_inpt)))
 
-        self.exprs.update(linear.exprs(
-            self.exprs['inpt'], P.in_to_out, 0, 'identity'))
-        self.exprs.update(sparsefiltering.loss(self.exprs['output'], self.feature_transfer))
+        parameters = ParameterSet()
+
+        self.affine = simple.AffineNonlinear(inpt, self.n_inpt, self.n_output, transfer='identity',
+            declare=parameters.declare)
+
+        output = self.affine.output
+
+        sparse_loss = sparse_filtering_loss(output, self.feature_transfer)['loss']
+
+        UnsupervisedModel.__init__(self, inpt=inpt,
+                                 output=output,
+                                 loss=sparse_loss,
+                                 parameters=parameters)
+
+        self.filters_in_to_hidden = parameters[self.affine.weights]
