6b047d7176871cd562178623f6fc83d9ba329756
Justin Bayer <bayer.justin@googlemail.com>
2015-06-29 12:44:16 +0200

    Revert "removed unused classes"
    This reverts commit 86bc213578553c97bcc99ad6fdbb2201c527dcb2.

--- breze/arch/construct/simple.py
+++ breze/arch/construct/simple.py
@@ -39,3 +39,58 @@ class AffineNonlinear(Layer):
         f = lookup(self.transfer, _transfer)
 
         self.output = f(self.output_in)
+
+
+class Split(Layer):
+
+    def __init__(self, inpt, lengths, axis=1, declare=None, name=None):
+        self.inpt = inpt
+        self.lengths = lengths
+        self.axis = axis
+        super(Split, self).__init__(declare, name)
+
+    def _forward(self):
+        starts = [0] + np.add.accumulate(self.lengths).tolist()
+        stops = starts[1:]
+        starts = starts[:-1]
+
+        self.outputs = [self.inpt[:, start:stop] for start, stop
+                        in zip(starts, stops)]
+
+
+class Concatenate(Layer):
+
+    def __init__(self, inpts, axis=1, declare=None, name=None):
+        self.inpts = inpts
+        self.axis = axis
+        super(Concatenate, self).__init__(declare, name)
+
+    def _forward(self):
+        concatenated = T.concatenate(self.inpts, self.axis)
+        self.output = concatenated
+
+
+class SupervisedLoss(Layer):
+
+    def __init__(self, target, prediction, loss, comp_dim=1, imp_weight=None,
+                 declare=None, name=None):
+        self.target = target
+        self.prediction = prediction
+        self.loss_ident = loss
+
+        self.imp_weight = imp_weight
+        self.comp_dim = comp_dim
+
+        super(SupervisedLoss, self).__init__(declare, name)
+
+    def _forward(self):
+        f_loss = lookup(self.loss_ident, _loss)
+
+        self.coord_wise = f_loss(self.target, self.prediction)
+
+        if self.imp_weight is not None:
+            self.coord_wise *= self.imp_weight
+
+        self.sample_wise = self.coord_wise.sum(self.comp_dim)
+
+        self.total = self.sample_wise.mean()
