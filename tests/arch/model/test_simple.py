35f65ec9d45008e0ccc3e14e797f3accdb0fb500
Justin Bayer <bayer.justin@googlemail.com>
2015-07-03 11:38:32 +0200

    fixed some tests

--- tests/arch/model/test_simple.py
+++ tests/arch/model/test_simple.py
@@ -6,12 +6,17 @@ import theano.tensor as T
 
 from breze.arch.construct.simple import AffineNonlinear
 from breze.arch.component.loss import squared
-from breze.arch.model.simple import SupervisedModel
+from breze.learn.base import SupervisedModel
+from breze.utils.testhelpers import use_test_values
 
 
+@use_test_values('raise')
 def test_linear_regression():
     inpt = T.matrix('inpt')
+    inpt.tag.test_value = np.zeros((3, 10))
+    inpt.tag.test_value
     target = T.matrix('target')
+    target.tag.test_value = np.zeros((3, 2))
 
     l = AffineNonlinear(inpt, 10, 2, 'tanh')
 
--- tests/arch/test_construct.py
+++ tests/arch/test_construct.py
@@ -5,40 +5,15 @@ import numpy as np
 
 import theano
 import theano.tensor as T
-from breze.arch.construct import base
 from breze.arch.construct.layer import simple, sequential
+from breze.utils.testhelpers import use_test_values
 
 
 X = np.zeros((10, 2)).astype(theano.config.floatX)
 Z = np.zeros((10, 3)).astype(theano.config.floatX)
 
 
-theano.config.compute_test_value = 'raise'
-
-
-def test_simple_stack():
-    inpt = T.matrix('inpt')
-    inpt.tag.test_value = X
-    target = T.matrix('target')
-    target.tag.test_value = Z
-
-    layers = [
-        simple.AffineNonlinear(2, 2),
-        simple.AffineNonlinear(2, 2),
-        simple.AffineNonlinear(2, 3),
-    ]
-    loss = simple.SupervisedLoss('squared', target=target)
-
-    s = base.SupervisedStack(layers=layers, loss=loss)
-
-    s.forward(inpt)
-    s._replace_param_dummies()
-
-    s.function(['inpt', 'target'], 'loss')
-    Y = s.predict(X)
-    assert Y.shape == (10, 3), 'shape of output not right'
-
-
+@use_test_values('raise')
 def test_sequential():
     inpt = T.tensor3('inpt')
     inpt.tag.test_value = np.zeros((2, 3, 4)).astype(theano.config.floatX)
@@ -50,41 +25,11 @@ def test_sequential():
     assert recons[0].tag.test_value.shape == (2, 3, 4)
 
 
-def test_rnn():
-    inpt = T.tensor3('inpt')
-    inpt.tag.test_value = np.empty((10, 5, 2)).astype(theano.config.floatX)
-
-    target = T.tensor3('target')
-    target.tag.test_value = np.empty((10, 5, 3)).astype(theano.config.floatX)
-
-    seq_to_static = sequential.SequentialToStatic()
-
-    layers = [
-        seq_to_static,
-        simple.AffineNonlinear(2, 2),
-        seq_to_static.inverse,
-        sequential.Recurrent(2, 'tanh'),
-        seq_to_static,
-        simple.AffineNonlinear(2, 3),
-        seq_to_static.inverse,
-    ]
-    loss = simple.SupervisedLoss('squared', target=target)
-
-    s = base.SupervisedStack(layers=layers, loss=loss)
-
-    s.forward(inpt)
-    s._replace_param_dummies()
-
-    s.function(['inpt', 'target'], 'loss')
-    X = np.zeros((10, 5, 2)).astype(theano.config.floatX)
-    Y = s.predict(X)
-    assert Y.shape == (10, 5, 3), 'shape of output not right'
-
-
+@use_test_values('raise')
 def test_supervised_loss():
     prediction = T.matrix('prediction')
-    y = prediction.tag.test_value = np.zeros((2, 3)).astype(theano.config.floatX)
-
+    y = prediction.tag.test_value = np.zeros((2, 3)
+                                             ).astype(theano.config.floatX)
 
     target = T.matrix('target')
     z = target.tag.test_value = prediction.tag.test_value.copy()
@@ -99,7 +44,7 @@ def test_supervised_loss():
     imp_weight = T.matrix('imp_weight')
     w = np.zeros_like(y).astype(theano.config.floatX)
     imp_weight.tag.test_value = w
-    w[0, 0,] = 1
+    w[0, 0] = 1
 
     loss_layer = simple.SupervisedLoss('squared', target, imp_weight=imp_weight)
     loss_expr, = loss_layer(prediction)
@@ -110,8 +55,7 @@ def test_supervised_loss():
     l = loss_expr.eval({prediction: y,
                         target: z,
                         imp_weight:
-                            np.zeros_like(w).astype(theano.config.floatX)
-})
+                            np.zeros_like(w).astype(theano.config.floatX)})
     assert l == 0, 'loss calculation with imp weights wrong'
 
     l = loss_expr.eval({prediction: y,
