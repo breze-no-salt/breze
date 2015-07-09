deff703daef2851abe187307c1029fcac043ee9e
Justin Bayer <bayer.justin@googlemail.com>
2015-07-09 10:28:47 +0200

    pep8 renamed p of Benroulli to rate

--- breze/arch/construct/layer/distributions.py
+++ breze/arch/construct/layer/distributions.py
@@ -3,10 +3,10 @@
 import theano.tensor as T
 import numpy as np
 
-from breze.arch.construct.base import Layer
 from breze.arch.component.varprop.loss import (
-    diag_gaussian_nll as diag_gauss_nll, bern_ces)
+    diag_gaussian_nll as diag_gauss_nll)
 from breze.arch.component.common import supervised_loss
+
 
 def assert_no_time(X):
     if X.ndim == 2:
@@ -15,13 +15,16 @@ def assert_no_time(X):
         raise ValueError('ndim must be 2 or 3, but it is %i' % X.ndim)
     return wild_reshape(X, (-1, X.shape[2]))
 
+
 def recover_time(X, time_steps):
     return wild_reshape(X, (time_steps, -1, X.shape[1]))
+
 
 def normal_logpdf(xs, means, vrs):
     energy = -(xs - means) ** 2 / (2 * vrs)
     partition_func = -T.log(T.sqrt(2 * np.pi * vrs))
     return partition_func + energy
+
 
 class Distribution(object):
 
@@ -37,20 +40,20 @@ class Distribution(object):
     def nll(self, X, inpt=None):
         raise NotImplemented()
 
+
 class DiagGauss(Distribution):
 
     def __init__(self, mean, var, rng=None):
         self.mean = mean
         self.var = var
-        self.stt = T.concatenate((mean,var),-1)
+        self.stt = T.concatenate((mean,var), -1)
         super(DiagGauss, self).__init__(rng)
 
     def sample(self, epsilon=None):
-
         mean_flat = assert_no_time(self.mean)
         var_flat = assert_no_time(self.var)
 
-        if epsilon == None:
+        if epsilon is None:
             noise = self.rng.normal(size=mean_flat.shape)
         else:
             noise = epsilon
@@ -71,13 +74,14 @@ class DiagGauss(Distribution):
         ll = weighted_squares - normalization
         return -ll
 
+
 class NormalGauss(Distribution):
 
     def __init__(self, shape, rng=None):
         self.shape = shape
         self.mean = T.zeros(shape)
         self.var = T.ones(shape)
-        self.stt = T.concatenate(( self.mean, self.var),-1)
+        self.stt = T.concatenate(( self.mean, self.var), -1)
         super(NormalGauss, self).__init__(rng)
 
     def sample(self):
@@ -88,23 +92,24 @@ class NormalGauss(Distribution):
         nll = -normal_logpdf(X_flat, self.mean, self.var)
         return nll.reshape(X.shape)
 
+
 class Bernoulli(Distribution):
 
-    def __init__(self, p, rng=None):
-        self.p = p
-        self.stt = p
+    def __init__(self, rate, rng=None):
+        self.rate = rate
+        self.stt = rate
         super(Bernoulli, self).__init__(rng)
 
     def sample(self, epsilon=None):
         if epsilon == None:
-            noise = rng.uniform(size=self.p.shape)
+            noise = rng.uniform(size=self.rate.shape)
         else:
             noise = epsilon
-        sample = noise < self.p
+        sample = noise < self.rate
         return sample
 
     def nll(self, X, inpt=None):
-        p = self.p
-        p *= 0.999
-        p += 0.0005
-        return -(X * T.log(p) + (1 - X) * T.log(1 - p))
+        rate = self.rate
+        rate *= 0.999
+        rate += 0.0005
+        return -(X * T.log(rate) + (1 - X) * T.log(1 - rate))
